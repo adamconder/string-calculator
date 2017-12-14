58fd5c10c392a6e14125bcef71fed57a574c06d8
Adam Conder <adamdconder@gmail.com>
2017-12-14 15:01:24 +0000

    Refactor toInt to return Array[Int]

--- src/main/scala/stringcalculator/StringCalculator.scala
+++ src/main/scala/stringcalculator/StringCalculator.scala
@@ -4,8 +4,8 @@ class StringCalculator(digits : String) {
 
   def add = {
     if(digits.length == 0) 0 else {
-      val numbers = digits split ","
-      (0 /: numbers)(_+_.toInt)
+      val numbers = digits split "," map(_.toInt)
+      (0 /: numbers)(_+_)
     }
   }
 
